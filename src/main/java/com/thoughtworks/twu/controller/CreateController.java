d8b59d6aa818f1effbed0cebcb5bc6b1a3df69ac
garima sharma <sharma_garima27@yahoo.com>
2012-10-25 09:21:52 +0530

    login improved/Garima and Qiushi

--- src/main/java/com/thoughtworks/twu/controller/CreateController.java
+++ src/main/java/com/thoughtworks/twu/controller/CreateController.java
@@ -10,17 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class CreateController {
+
     private UserService userService;
 
     @Autowired
-    public HomeController(UserService userService) {
+    public CreateController(UserService userService) {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "?ticket", method = RequestMethod.GET)
     public ModelAndView homepage(
-        @RequestParam(value = "username", defaultValue = "") String username) {
+            @RequestParam(value = "username", defaultValue = "") String username) {
         ModelAndView modelAndView = new ModelAndView("example/home");
         if (!username.isEmpty()) {
             User user = userService.getUser(username);
