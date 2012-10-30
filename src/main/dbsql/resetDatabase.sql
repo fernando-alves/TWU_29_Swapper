b2d2c4dce67d3de5c885a417e32ee0b49b319ce6
Ryan Tessier <r.tessier@live.com.au>
2012-10-30 17:43:05 +0530

    F&R fix the db reset issue

--- src/main/dbsql/resetDatabase.sql
+++ src/main/dbsql/resetDatabase.sql
@@ -1,12 +1,13 @@
-DROP DATABASE IF EXISTS Swap;
-CREATE DATABASE Swap;
-USE Swap;
+DROP DATABASE IF EXISTS swap;
+CREATE DATABASE swap;
+Use swap;
 
-CREATE TABLE ChangeLog (
+CREATE TABLE changeLog (
   change_number BIGINT NOT NULL,
   complete_dt TIMESTAMP NOT NULL,
   applied_by VARCHAR(100) NOT NULL,
   description VARCHAR(500) NOT NULL
 );
 
-ALTER TABLE ChangeLog ADD CONSTRAINT pkChangeLog PRIMARY KEY (change_number);
+
+ALTER TABLE changeLog ADD CONSTRAINT pkChangeLog PRIMARY KEY (change_number);
