b2c5ce520dc07b1d36a224c108c86a9611b0c4d4
ccyutw <ccyutw@thoughtworks.com>
2012-10-31 14:59:01 +0530

    add functional Test for login

--- src/test/java/functional/com/thoughtworks/twu/LoginFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/LoginFunctionalTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,7 +20,30 @@ public class LoginFunctionalTest {
     public void setUp() {
         webDriver = new FirefoxDriver();
     }
+    @Test
+    public void shouldShowErrorWhenEmptyFields() {
+        webDriver.get("https://cas.thoughtworks.com/cas/login");
+        webDriver.findElement(By.name("submit")).click();
 
+        WebElement errorMsgElement = webDriver.findElement(By.id("msg"));
+
+        String expectedErrorMsg = "Password is a required field.\n" +
+                "Username is a required field.";
+        assertThat(errorMsgElement.getText(), is(expectedErrorMsg));
+    }
+
+    @Test
+    public void shouldShowErrorMsgWhenInvalidCredentials() {
+        webDriver.get("https://cas.thoughtworks.com/cas/login");
+        webDriver.findElement(By.id("username")).sendKeys("blah");
+        webDriver.findElement(By.id("password")).sendKeys("blah");
+        webDriver.findElement(By.name("submit")).click();
+
+        WebElement errorMsgElement = webDriver.findElement(By.id("msg"));
+
+        String expectedErrorMsg = "The credentials you provided cannot be determined to be authentic.";
+        assertThat(errorMsgElement.getText(), is(expectedErrorMsg));
+    }
     @Test
     public void shouldLoginSuccessfully(){
         webDriver.get("http://127.0.0.1:8080/twu/");
@@ -33,6 +57,23 @@ public class LoginFunctionalTest {
         String expectedUrl = webDriver.getCurrentUrl();
 
         assertThat(expectedUrl.contains("http://127.0.0.1:8080/twu"), is(true));
+
+    }
+    public  void shouldBeAbleToPerformSSO() {
+        webDriver.get("http://castest.thoughtworks.com");
+        String username = "test.twu";
+        String password = "Th0ughtW0rks@12";
+
+        webDriver.findElement(By.id("username")).sendKeys(username);
+        webDriver.findElement(By.id("password")).sendKeys(password);
+        webDriver.findElement(By.name("submit")).click();
+        webDriver.get("http://127.0.0.1:8080/twu/");
+
+        String expectedUrl = webDriver.getCurrentUrl();
+
+        assertThat(expectedUrl.contains("http://127.0.0.1:8080/twu"), is(true));
+
+        webDriver.getPageSource().contains("test.twu");
 
     }
 
