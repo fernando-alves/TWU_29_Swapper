50405a8881b89b0ad516a4f6e4d523fa1353f70e
dnikitadeshmukh <dnikitadeshmukh@gmail.com>
2012-11-06 11:38:01 +0530

    Removed unneeded functional test. (Nikita/Ryan)

--- src/test/java/functional/com/thoughtworks/twu/CreateOfferFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/CreateOfferFunctionalTest.java
@@ -28,6 +28,18 @@ public class CreateOfferFunctionalTest {
     }
 
     @Test
+    public void showGoToCreateOfferPage() throws InterruptedException {
+        logIn();
+
+        Thread.sleep(1000);
+        webDriver.findElement(By.id("createOffer")).click();
+
+        String expectedUrl = "http://127.0.0.1:8080/twu/offer/create";
+        assertThat(webDriver.getCurrentUrl(), is(expectedUrl));
+
+    }
+
+    @Test
     public void showGoToViewAnOfferPage() throws InterruptedException {
         logIn();
 
