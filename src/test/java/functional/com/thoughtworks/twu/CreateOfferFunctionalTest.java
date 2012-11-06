9d6531f21c71ce8414361c47f9d372cf2e6c3380
garima sharma <sharma_garima27@yahoo.com>
2012-11-06 16:13:55 +0530

    functional test for browse updated

---
+++ src/test/java/functional/com/thoughtworks/twu/CreateOfferFunctionalTest.java
@@ -0,0 +1,139 @@
+package functional.com.thoughtworks.twu;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class CreateOfferFunctionalTest {
+
+
+    private WebDriver webDriver;
+    private String username;
+    private String password;
+
+    @Before
+    public void setUp() {
+        webDriver = new FirefoxDriver();
+        username = "test.twu";
+        password = "Th0ughtW0rks@12";
+    }
+
+    @Test
+    public void shouldGoToCreateOfferPage() throws InterruptedException {
+        logIn();
+
+        Thread.sleep(1000);
+        webDriver.findElement(By.id("createOffer")).click();
+
+        String expectedUrl = "http://127.0.0.1:8080/twu/offer/create";
+        assertThat(webDriver.getCurrentUrl(), is(expectedUrl));
+
+    }
+
+    @Test
+    public void shouldGoToViewAnOfferPage() throws InterruptedException {
+        logIn();
+
+        Thread.sleep(1000);
+        webDriver.findElement(By.id("createOffer")).click();
+        webDriver.findElement(By.name("title")).sendKeys("TITLE IN TEST");
+
+        Select select = new Select(webDriver.findElement(By.tagName("select")));
+        select.selectByValue("Cars");
+
+        webDriver.findElement(By.name("description")).sendKeys("To pass the test or not, this is a question");
+        webDriver.findElement(By.name("submit")).click();
+        Thread.sleep(1000);
+        String expectedUrl = "http://127.0.0.1:8080/twu/offer/viewAnOfferAfterCreating?username=test.twu";
+        assertThat(expectedUrl, is(webDriver.getCurrentUrl()));
+    }
+
+    @Test
+    public void showGoToViewAnOfferPageFromBrowse() throws InterruptedException {
+        logIn();
+
+        Thread.sleep(1000);
+        webDriver.findElement(By.id("browse")).click();
+
+        webDriver.findElement(By.id("offer1")).click();
+        Thread.sleep(1000);
+        assertTrue(webDriver.getCurrentUrl().contains("http://127.0.0.1:8080/twu/offer/viewAnOfferFromBrowse?offerId="));
+    }
+
+    @Test
+    public void shouldHaveAnElementToEnterDescription() {
+        logIn();
+
+        webDriver.findElement(By.id("createOffer")).click();
+        WebElement titleTextBox = webDriver.findElement(By.name("description"));
+
+        assertThat(titleTextBox.isDisplayed(), is(true));
+    }
+
+    @Test
+    public void shouldHaveAnElementToSelectCategory() {
+        logIn();
+
+        webDriver.findElement(By.id("createOffer")).click();
+        WebElement categoryList = webDriver.findElement(By.name("category"));
+
+        assertThat(categoryList.isDisplayed(), is(true));
+    }
+
+    @Test
+    public void shouldHaveAnElementToEnterTitle() {
+        logIn();
+
+        webDriver.findElement(By.id("createOffer")).click();
+        WebElement titleTextBox = webDriver.findElement(By.name("title"));
+
+        assertThat(titleTextBox.isDisplayed(), is(true));
+    }
+
+    @Test
+    public void shouldCheckExistenceOfCreateOfferLink() {
+        logIn();
+
+        WebElement clickButton = webDriver.findElement(By.id("createOffer"));
+        assertNotNull(clickButton);
+    }
+@Test
+    public void shouldCheckExistenceOfBrowseOfferLink() {
+        logIn();
+
+        WebElement clickButton = webDriver.findElement(By.id("browse"));
+        assertNotNull(clickButton);
+    }
+
+    @Test
+    public void shouldDisplayUsername() {
+        logIn();
+
+        WebElement userNameOnCreatePage = webDriver.findElement(By.id("username"));
+
+        assertThat(userNameOnCreatePage.getText().contains(username), is(true));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        webDriver.close();
+    }
+
+    private void logIn() {
+        webDriver.get("http://127.0.0.1:8080/twu/");
+
+        webDriver.findElement(By.id("username")).sendKeys(username);
+        webDriver.findElement(By.id("password")).sendKeys(password);
+        webDriver.findElement(By.name("submit")).click();
+    }
+}
